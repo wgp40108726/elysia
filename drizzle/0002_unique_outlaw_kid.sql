71c43e24559536a95d20e7184baedab76dde457c
Developer <dev@breakfast.com>
2026-05-21 16:43:46 +0800

    fix-v9-database-migration-script

--- drizzle/0002_unique_outlaw_kid.sql
+++ drizzle/0002_unique_outlaw_kid.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "bf_v9"."users" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
 DROP TABLE "bf_v9"."users" CASCADE;--> statement-breakpoint
-ALTER TABLE "bf_v9"."orders" DROP CONSTRAINT "orders_user_id_users_id_fk";
+ALTER TABLE "bf_v9"."orders" DROP CONSTRAINT IF EXISTS "orders_user_id_users_id_fk";
 --> statement-breakpoint
 ALTER TABLE "bf_v9"."orders" ADD CONSTRAINT "orders_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "bf_v9"."user"("id") ON DELETE no action ON UPDATE no action;
